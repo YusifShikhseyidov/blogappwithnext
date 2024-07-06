3de42f1d26b014766542e6a1c11e6b3a67886f58
Yusif Shikhseyidov <yusif1580@gmail.com>
2024-07-06 19:01:04 +0400

    Create navbar(logo&dark&light switch)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ import Link from "next/link"
 import { ThemeSwitcherButton } from "./ThemeSwitcherButton"
 import { Changa_One } from "next/font/google";
 
-const tiChny = Changa_One({ subsets: ["latin"], weight: ["400"] })
+const logoSpecificFont = Changa_One({ subsets: ["latin"], weight: ["400"] })
 
 export const Navbar = () => {
   return (
     <nav className="mx-auto max-w-5xl px-6 bg-transparent">
       <div className="w-full h-16 flex justify-between items-center">
         <Link href='/'>
-          <span className={`${tiChny.className} text-[#1da1f2] text-3xl`}>Demo</span>
+          <span className={`${logoSpecificFont.className} text-[#1da1f2] text-3xl`}>Demo</span>
           Blog
         </Link>
         <div>
